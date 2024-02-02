4b640dd26617b9ea8766fa0ea8d22eb09d52d833
Kourtneet <147271911+Kourtneet@users.noreply.github.com>
2024-02-02 00:13:42 -0500

    Add files via upload

---
+++ Crowfunding_ETL/Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,4 @@
+SELECT * FROM crowdfunding_db_schema."Subcategory"
+SELECT * FROM crowdfunding_db_schema."Contact"
+SELECT * FROM crowdfunding_db_schema."Category"
+SELECT * FROM crowdfunding_db_schema."Campaign"
